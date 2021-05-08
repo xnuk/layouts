aff23b469710dae2fd2eb52e196e89a9e145650f
Xnuk Shuman <admin@xnu.kr>
2021-05-09 05:01:41 +0900

    안 씹히게

--- keymap.c
+++ keymap.c
@@ -220,9 +220,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-	if (clockwise) {
-		tap_code(KC_AUDIO_VOL_UP);
-	} else {
-		tap_code(KC_AUDIO_VOL_DOWN);
+#ifdef CONSOLE_ENABLE
+	dprintf("encoder: %d %d\n", index, clockwise);
+#endif
+
+	if (index == 0) {
+		if (clockwise) {
+			tap_code_delay(KC_AUDIO_VOL_UP, 80);
+		} else {
+			tap_code_delay(KC_AUDIO_VOL_DOWN, 80);
+		}
 	}
 }
