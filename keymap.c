f5df301ff08570f6663f00cf6058c2d8337bae97
즈눅 <xnuk@users.noreply.github.com>
2021-12-11 03:18:37 +0900

    no arrow key in game mod

--- keymap.c
+++ keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 , KC_TAB               , KC_Q      , KC_W      , KC_F      , KC_P    , KC_B     , KC_J     , KC_L    , KC_U      , KC_Y     , KC_SCLN   , KC_BSPACE
 , KC_ESC               , KC_A      , KC_R      , KC_S      , KC_T    , KC_G     , KC_M     , KC_N    , KC_E      , KC_I     , KC_O      , KC_MINS
 , KC_LSFT              , KC_Z      , KC_X      , KC_C      , KC_D    , KC_V     , KC_K     , KC_H    , KC_COMMA  , KC_DOT   , KC_SLASH  , KC_ENTER
-, KC_LGUI              , KC_LALT   , KC_LCTRL  , KC_BSLS   , KC_LSFT , KC_SPC   , KC_LBRC  , KC_RSFT , KC_LEFT   , KC_DOWN  , KC_UP     , KC_RIGHT
+, KC_LGUI              , KC_LALT   , KC_LCTRL  , KC_BSLS   , KC_LSFT , KC_SPC   , KC_LBRC  , KC_RSFT , KC_RBRC   , KC_RCTRL , KC_RALT   , KC_EQL
 )
 
 , [Layer_qwerty] = LAYOUT_ortho_5x12
