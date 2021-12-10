60449f574200ce0b8aac94c10c35ab7e4f40fb97
즈눅 <xnuk@users.noreply.github.com>
2021-12-11 01:42:03 +0900

    흠므

--- keymap.c
+++ keymap.c
@@ -111,8 +111,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 ( KC_AUDIO_MUTE    , KC_1      , KC_2      , KC_3      , KC_4                          , KC_5     , KC_6     , KC_7                           , KC_8                      , KC_9      , KC_0      , KC_BSPACE
 , LALT_T(KC_TAB)   , KC_Q      , KC_W      , KC_E      , KC_R                          , KC_T     , KC_Y     , KC_U                           , KC_I                      , KC_O      , KC_P      , KC_BSPACE
 , LCTL_T(KC_ESC)   , KC_A      , KC_S      , KC_D      , KC_F                          , KC_G     , KC_H     , KC_J                           , KC_K                      , KC_L      , KC_SCLN   , KC_MINS
-, KC_LSPO          , KC_Z      , KC_X      , KC_C      , KC_V                          , KC_B     , KC_N     , KC_M                           , KC_COMMA                  , KC_DOT    , KC_SLASH  , KC_ENTER
-, ALL_T(KC_BSLS)   , KC_LALT   , KC_LCTRL  , KC_LGUI   , LT(Layer_left_mod,KC_RIGHT)   , KC_SPC   , KC_SPC   , LT(Layer_right_mod,KC_HAEN)    , LT(Layer_mouse,KC_LEFT)   , KC_DOWN   , KC_UP     , KC_RIGHT
+, _                , KC_Z      , KC_X      , KC_C      , KC_V                          , KC_B     , KC_N     , KC_M                           , KC_COMMA                  , KC_DOT    , KC_SLASH  , KC_ENTER
+, ALL_T(KC_BSLS)   , _         , _         , _         , _                             , KC_SPC   , KC_SPC   , _                              , _                         , _         , _         , _
 )
 
 
