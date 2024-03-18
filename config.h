764a725ccf72c5924253a3e10d2f123d16920107
Xnuk Shuman <admin@xnu.kr>
2024-03-19 03:11:17 +0900

    NKRO

--- config.h
+++ config.h
@@ -20,27 +20,6 @@ fuck this shit imma out;
 
 #endif
 
-#ifdef AUDIO_ENABLE
-#define STARTUP_SONG SONG(PREONIC_SOUND)
-
-// #define AUDIO_CLICKY
-// #define AUDIO_CLICKY_FREQ_RANDOMNESS 0.5f
-// #define AUDIO_CLICKY_FREQ_MIN 70.0f
-
-/*
-#define DEFAULT_LAYER_SONGS { \
-SONG(QWERTY_SOUND), \
-SONG(COLEMAK_SOUND), \
-SONG(DVORAK_SOUND) \
-}*/
-
-/*
-#define AUDIO_ENABLE_TONE_MULTIPLEXING
-#define AUDIO_TONE_MULTIPLEXING_RATE_DEFAULT 10
-*/
-#endif
-
-#define MUSIC_MASK (keycode != KC_NO)
 #define NO_MUSIC_MODE
 #ifdef AUDIO_CLICKY
 #undef AUDIO_CLICKY
@@ -49,17 +28,6 @@ SONG(DVORAK_SOUND) \
 #ifdef AUTO_SHIFT_MODIFIERS
 #undef
 #endif
-
-/* enable advanced MIDI features:
-   - MIDI notes can be added to the keymap
-   - Octave shift and transpose
-   - Virtual sustain, portamento, and modulation wheel
-   - etc.
-*/
-//#define MIDI_ADVANCED
-
-/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
-//#define MIDI_TONE_KEYCODE_OCTAVES 2
 
 #define TAPPING_TERM 150
 #define PERMISSIVE_HOLD
@@ -99,3 +67,5 @@ SONG(DVORAK_SOUND) \
 #define USB_POLLING_INTERVAL_MS 1
 
 #define USB_MAX_POWER_CONSUMPTION 100
+
+#define FORCE_NKRO
