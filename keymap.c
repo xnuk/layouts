1ea54990da5fceca6b2f8df77ce3d0951c446a99
Xnuk Shuman <admin@xnu.kr>
2024-03-18 22:01:42 +0900

    애초에 이름을 똑바로 지었으면 됐잖아 이것들아

--- keymap.c
+++ keymap.c
@@ -52,27 +52,27 @@ enum preonic_layers
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 { [Layer_default] = LAYOUT_ortho_5x12
-( KC_AUDIO_MUTE    , KC_1      , KC_2      , KC_3      , KC_4                          , KC_5     , KC_6     , KC_7                           , KC_8                      , KC_9      , KC_0      , KC_BSPACE
-, LALT_T(KC_TAB)   , KC_Q      , KC_W      , KC_F      , KC_P                          , KC_B     , KC_J     , KC_L                           , KC_U                      , KC_Y      , KC_SCLN   , KC_BSPACE
+( KC_AUDIO_MUTE    , KC_1      , KC_2      , KC_3      , KC_4                          , KC_5     , KC_6     , KC_7                           , KC_8                      , KC_9      , KC_0      , KC_BSPC
+, LALT_T(KC_TAB)   , KC_Q      , KC_W      , KC_F      , KC_P                          , KC_B     , KC_J     , KC_L                           , KC_U                      , KC_Y      , KC_SCLN   , KC_BSPC
 , LCTL_T(KC_ESC)   , KC_A      , KC_R      , KC_S      , KC_T                          , KC_G     , KC_M     , KC_N                           , KC_E                      , KC_I      , KC_O      , KC_MINS
-, KC_LSPO          , KC_Z      , KC_X      , KC_C      , KC_D                          , KC_V     , KC_K     , KC_H                           , KC_COMMA                  , KC_DOT    , KC_SLASH  , KC_ENTER
-, ALL_T(KC_BSLS)   , KC_LALT   , KC_LCTRL  , KC_LGUI   , LT(Layer_left_mod,KC_RIGHT)   , KC_SPC   , KC_SPC   , LT(Layer_right_mod,KC_HAEN)    , LT(Layer_mouse,KC_LEFT)   , KC_DOWN   , KC_UP     , KC_RIGHT
+, SC_LSPO          , KC_Z      , KC_X      , KC_C      , KC_D                          , KC_V     , KC_K     , KC_H                           , KC_COMMA                  , KC_DOT    , KC_SLASH  , KC_ENTER
+, ALL_T(KC_BSLS)   , KC_LALT   , KC_LCTL  , KC_LGUI   , LT(Layer_left_mod,KC_RIGHT)   , KC_SPC   , KC_SPC   , LT(Layer_right_mod,KC_LANG1)    , LT(Layer_mouse,KC_LEFT)   , KC_DOWN   , KC_UP     , KC_RIGHT
 )
 
 , [Layer_game] = LAYOUT_ortho_5x12
 ( TO(Layer_default)    , KC_1      , KC_2      , KC_3      , KC_4    , KC_5     , KC_6     , KC_7    , KC_8      , KC_9     , KC_0      , TO(Layer_default)
-, KC_TAB               , KC_Q      , KC_W      , KC_F      , KC_P    , KC_B     , KC_J     , KC_L    , KC_U      , KC_Y     , KC_SCLN   , KC_BSPACE
+, KC_TAB               , KC_Q      , KC_W      , KC_F      , KC_P    , KC_B     , KC_J     , KC_L    , KC_U      , KC_Y     , KC_SCLN   , KC_BSPC
 , KC_ESC               , KC_A      , KC_R      , KC_S      , KC_T    , KC_G     , KC_M     , KC_N    , KC_E      , KC_I     , KC_O      , KC_MINS
 , KC_LSFT              , KC_Z      , KC_X      , KC_C      , KC_D    , KC_V     , KC_K     , KC_H    , KC_COMMA  , KC_DOT   , KC_SLASH  , KC_ENTER
-, KC_LGUI              , KC_LALT   , KC_LCTRL  , KC_BSLS   , KC_LSFT , KC_SPC   , KC_LBRC  , KC_RSFT , KC_RBRC   , KC_RCTRL , KC_RALT   , KC_EQL
+, KC_LGUI              , KC_LALT   , KC_LCTL  , KC_BSLS   , KC_LSFT , KC_SPC   , KC_LBRC  , KC_RSFT , KC_RBRC   , KC_RCTRL , KC_RALT   , KC_EQL
 )
 
 , [Layer_qwerty] = LAYOUT_ortho_5x12
-( KC_AUDIO_MUTE    , KC_1      , KC_2      , KC_3      , KC_4                          , KC_5     , KC_6     , KC_7                           , KC_8                      , KC_9      , KC_0      , KC_BSPACE
-, LALT_T(KC_TAB)   , KC_Q      , KC_W      , KC_E      , KC_R                          , KC_T     , KC_Y     , KC_U                           , KC_I                      , KC_O      , KC_P      , KC_BSPACE
+( KC_AUDIO_MUTE    , KC_1      , KC_2      , KC_3      , KC_4                          , KC_5     , KC_6     , KC_7                           , KC_8                      , KC_9      , KC_0      , KC_BSPC
+, LALT_T(KC_TAB)   , KC_Q      , KC_W      , KC_E      , KC_R                          , KC_T     , KC_Y     , KC_U                           , KC_I                      , KC_O      , KC_P      , KC_BSPC
 , LCTL_T(KC_ESC)   , KC_A      , KC_S      , KC_D      , KC_F                          , KC_G     , KC_H     , KC_J                           , KC_K                      , KC_L      , KC_SCLN   , KC_MINS
 , KC_LSPO          , KC_Z      , KC_X      , KC_C      , KC_V                          , KC_B     , KC_N     , KC_M                           , KC_COMMA                  , KC_DOT    , KC_SLASH  , KC_ENTER
-, ALL_T(KC_BSLS)   , KC_LALT   , KC_LCTRL  , KC_LGUI   , LT(Layer_left_mod,KC_RIGHT)   , KC_SPC   , KC_SPC   , LT(Layer_right_mod,KC_HAEN)    , LT(Layer_mouse,KC_LEFT)   , KC_DOWN   , KC_UP     , KC_RIGHT
+, ALL_T(KC_BSLS)   , KC_LALT   , KC_LCTL  , KC_LGUI   , LT(Layer_left_mod,KC_RIGHT)   , KC_SPC   , KC_SPC   , LT(Layer_right_mod,KC_LANG1)    , LT(Layer_mouse,KC_LEFT)   , KC_DOWN   , KC_UP     , KC_RIGHT
 )
 
 , [Layer_left_mod] = LAYOUT_ortho_5x12
