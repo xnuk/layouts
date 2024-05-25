0c3ab5defd22704a3aae95728d50fe1edce37a97
Xnuk Shuman <admin@xnu.kr>
2024-05-25 17:19:20 +0900

    color layer

--- keymaps/xnuk/keymap.c
+++ keymaps/xnuk/keymap.c
@@ -85,6 +85,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 
 #undef _
 
+__attribute__ ((weak))
+const rgblight_segment_t PROGMEM colo_upper[] = RGBLIGHT_LAYER_SEGMENTS
+( {7, 3, HSV_BLUE}
+, {13, 1, HSV_GREEN}
+, {14, 3, HSV_BLUE}
+, {19, 4, HSV_BLUE}
+);
+
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST
+( colo_upper
+);
+
+void keyboard_post_init_user(void) {
+	rgblight_layers = my_rgb_layers;
+}
+
 void matrix_output_unselect_delay(uint8_t line, bool key_pressed) {
     waitInputPinDelay();
     waitInputPinDelay();
@@ -93,5 +109,8 @@ void matrix_output_unselect_delay(uint8_t line, bool key_pressed) {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     state = update_tri_layer_state(state, Layer_lower, Layer_upper, Layer_adjust);
+
+    rgblight_set_layer_state(0, layer_state_cmp(state, Layer_upper));
+
     return state;
 }
